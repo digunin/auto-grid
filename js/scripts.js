5243b4758b31cdc56d8d6d62b88c26853cff77d9
digunin <kandidad@gmail.com>
2021-10-26 17:35:41 +0300

    react here

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-// setTimeout(() => {
-//   console.log('start')
-//   document.body.style.backgroundColor = 'green'
-//   console.log('end')
-// }, 2000)
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 5500,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|png|jpg|jpeg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
