0a93395aba72c1860bb0d40fae82b18ad524a2e9
digunin <kandidad@gmail.com>
2021-11-28 16:39:19 +0300

    edit sublime-block size. new default settings

--- js/components/settings/needPrint.js
+++ js/components/settings/needPrint.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import useSettings from '../useSettings'
 
-const NeedPrint = ({ side, onchange }) => {
+const NeedPrint = ({ onchange }) => {
   const { needPrint } = useSettings()
   return (
     <div
