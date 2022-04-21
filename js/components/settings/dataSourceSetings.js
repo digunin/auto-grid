731d28b1b6221e50826881eefeafac1d4701534c
dima <kandidad@gmail.com>
2022-04-21 20:04:23 +0300

    dataSourceList now clickable

--- js/components/settings/dataSourceSetings.js
+++ js/components/settings/dataSourceSetings.js
@@ -6,14 +6,23 @@ import useSettings from '../useSettings'
 const DataSourceSettings = () => {
   let {
     data_source: { editing_source_name, data },
-    actions: { setDataSource },
+    actions: { setDataSource, editDataSource },
   } = useSettings()
 
   let source_names = Object.keys(data)
 
+  let onSourceListClick = (name = null) => {
+    console.log(name)
+    editDataSource(name ? name : '')
+  }
+
   return (
     <>
-      <DataSourceList names={source_names} />
+      <DataSourceList
+        editing_source_name={editing_source_name}
+        onclick={onSourceListClick}
+        names={source_names}
+      />
       <DataPicker
         setDataSource={setDataSource}
         source_names={source_names}
--- js/components/settings/pickers/data-source/dataSourceList.js
+++ js/components/settings/pickers/data-source/dataSourceList.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
-const DataSourceList = ({ names }) => {
+const DataSourceList = ({ names, editing_source_name, onclick }) => {
   return (
     <div>
       {names.map((name, number) => {
+        let bgcolor =
+          name === editing_source_name ? 'var(--active-color)' : 'inherit'
         return (
           <div
-            style={{ textAlign: 'center' }}
+            style={{
+              textAlign: 'center',
+              backgroundColor: bgcolor,
+              padding: '0.5em',
+              margin: '0.5em',
+              borderRadius: '0.5em',
+            }}
             key={`source_list_item_${number}`}
+            onClick={() => onclick(name)}
           >{`${number + 1}. ${name}`}</div>
         )
       })}
+      <button onClick={() => onclick()}>Создать источних данных</button>
     </div>
   )
 }
