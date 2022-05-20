58741d7f8516a95d79e67c48030f6dd067086e6f
unknown <kandidad@gmail.com>
2022-05-20 13:06:53 +0300

    [FIX] set default values in useDataSource for correct loading old saved settings

--- js/components/settings/pickers/data-source/useDataSource.js
+++ js/components/settings/pickers/data-source/useDataSource.js
@@ -5,19 +5,22 @@ const useDataSource = (name = '') => {
   const { actions, data_source } = useContext(Context)
 
   return {
-    editing_source_name: data_source.editing_source_name,
-    existingNames: Object.keys(data_source.sources),
-    createMode: data_source.editing_source_name === '',
-    editMode: data_source.editing_source_name !== '',
+    editing_source_name: data_source?.editing_source_name || '',
+    existingNames: Object.keys(data_source?.sources || {}),
+    createMode: data_source?.editing_source_name === '',
+    editMode: data_source?.editing_source_name !== '',
     setDataSource: actions.setDataSource,
     editDataSource: actions.editDataSource,
     deleteDataSource: actions.deleteDataSource,
     editingData:
-      data_source.editing_source_name === ''
+      data_source?.editing_source_name === '' ||
+      !data_source?.editing_source_name
         ? []
-        : [...data_source.sources[data_source.editing_source_name].data],
+        : data_source.sources[data_source.editing_source_name].data || [],
     selectedDataSource:
-      name === '' ? { data: [] } : { ...data_source.sources[name] },
+      name === '' || !data_source?.sources
+        ? { data: [] }
+        : { ...data_source.sources[name] },
   }
 }
 
