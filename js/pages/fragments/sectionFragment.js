85acb64cd09ae69b8a35eb11a89eb2796c1fa794
unknown <kandidad@gmail.com>
2023-02-21 14:42:59 +0300

    rework section fragment

--- js/pages/fragments/sectionFragment.js
+++ js/pages/fragments/sectionFragment.js
@@ -2,17 +2,32 @@ import React, { useState } from 'react'
 
 const SectionFragment = ({ title = '', children }) => {
   const [isShow, setIsShow] = useState(false)
-  const isShowToggle = () => setIsShow((prev) => !prev)
-  const buttonText = isShow ? '▲' : '▼'
+
+  const isShowToggle = (event) => {
+    let content = event.currentTarget.parentElement.nextElementSibling
+    content.style.maxHeight = isShow ? null : content.scrollHeight + 'px'
+    setIsShow((prev) => !prev)
+  }
+
   return (
     <section width="100%">
-      <div className="header">
-        <h2>{title}</h2>
-        <button onClick={isShowToggle}>{buttonText}</button>
-      </div>
-      <div className={`section-content ${isShow ? 'show' : ''}`}>
-        {children}
-      </div>
+      <h2 className="header">
+        <button onClick={isShowToggle}>
+          <span>{title}</span>
+          <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+            <rect
+              className={isShow ? '' : 'vert'}
+              height="2"
+              width="8"
+              y="4"
+              x="1"
+            />
+            <rect height="2" width="8" y="4" x="1" />
+          </svg>
+        </button>
+      </h2>
+
+      <div className="section-content">{children}</div>
     </section>
   )
 }
