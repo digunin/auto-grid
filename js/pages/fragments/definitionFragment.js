15fcf02e5cf9442e4799c2d8da9392cfea5ab7f0
dima <kandidad@gmail.com>
2023-03-02 19:29:49 +0300

    additional improvement of the 'about' page appearance

--- js/pages/fragments/definitionFragment.js
+++ js/pages/fragments/definitionFragment.js
@@ -3,10 +3,8 @@ import React from 'react'
 const DefinitionFragment = ({ term, children }) => {
   return (
     <div className="def-fragment">
-      <em>
-        <b>{term}</b>
-      </em>{' '}
-      - {children}
+      <em>{`${term} - `}</em>
+      {children}
     </div>
   )
 }
